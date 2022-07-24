df46ca5a34cd7d4f1196c11d2154ce9334fbb10e
yonghui <iii996@qq.com>
2022-07-24 11:45:45 +0800

    1

---
+++ src/api/demo/account.ts
@@ -0,0 +1,23 @@
+import { defHttp } from '/@/utils/http/axios';
+
+interface GetAccountInfoModel {
+  email: string;
+  name: string;
+  introduction: string;
+  phone: string;
+  address: string;
+}
+
+enum Api {
+  ACCOUNT_INFO = '/account/getAccountInfo',
+  SESSION_TIMEOUT = '/user/sessionTimeout',
+  TOKEN_EXPIRED = '/user/tokenExpired',
+}
+
+// Get personal center-basic settings
+
+export const accountInfoApi = () => defHttp.get<GetAccountInfoModel>({ url: Api.ACCOUNT_INFO });
+
+export const sessionTimeoutApi = () => defHttp.post<void>({ url: Api.SESSION_TIMEOUT });
+
+export const tokenExpiredApi = () => defHttp.post<void>({ url: Api.TOKEN_EXPIRED });
